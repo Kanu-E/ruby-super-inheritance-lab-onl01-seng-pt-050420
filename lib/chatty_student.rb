8307e11032f7a62af182abea0791cd6fd0cd71d0
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-15 00:49:18 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,11 +2,11 @@ class Chattystudent < student
   
   def hello
    super
-    puts "Hey there! I'm so excited to learn stuff."
-  end
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    end
   
   def raise_hand
-    puts "Pick me!"
+    puts 10.times do 
   end
   
 end
